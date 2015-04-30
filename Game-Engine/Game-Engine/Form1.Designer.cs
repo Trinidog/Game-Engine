76faceddeb31444f3bdc73dcd48e64fdd5e17775
Trinidog <robertj_stevens@yahoo.com>
2015-04-29 18:37:47 -0600

    Graphics: Line Animation

--- Game-Engine/Game-Engine/Form1.Designer.cs
+++ Game-Engine/Game-Engine/Form1.Designer.cs
@@ -29,11 +29,43 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.screen = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(704, 442);
+            this.screen.TabIndex = 0;
+            this.screen.Paint += new System.Windows.Forms.PaintEventHandler(this.screen_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(704, 442);
+            this.Controls.Add(this.screen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Game-Engine";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Game-Engine/Game-Engine/Form1.cs
+++ Game-Engine/Game-Engine/Form1.cs
@@ -12,9 +12,32 @@ namespace Game_Engine
 {
     public partial class Form1 : Form
     {
+       
+      
+
+        private int x = 10;
         public Form1()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.DoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
         }
+       
+        private void screen_Paint(object sender, PaintEventArgs e)
+        {
+            this.DoubleBuffered = true;
+            Graphics gObject = screen.CreateGraphics();
+
+            Brush red = new SolidBrush(Color.Red);
+            Pen redPen = new Pen(red, 8);
+
+            gObject.DrawLine(redPen, x, 10, 400, 376);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x += 5;
+            screen.Invalidate();
+        } 
     }
+
 }
